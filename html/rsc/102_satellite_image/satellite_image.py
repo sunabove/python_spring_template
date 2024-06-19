3ac5d48cf3813d3a8091c50e5f3be73936c191c7
sunabove <sunabove@nate.com>
2024-06-19 21:28:04 +0900

    **

--- html/rsc/102_satellite_image/satellite_image.py
+++ html/rsc/102_satellite_image/satellite_image.py
@@ -67,6 +67,7 @@ for package in [ "requests", "pathlib" ]  :
 pass
 
 import requests
+from time import sleep
 from pathlib import Path
 from datetime import datetime, timedelta
 
@@ -83,35 +84,32 @@ class AttrDict:
     pass
 pass
 
-def load_satellite_image( api_key ) : 
+def load_satellite_image( api_key, area="ko", img_gb="ir105" ) : 
+
+    debug = 0
 
     # 현재 소스 폴더
     srcDir = Path( __file__ ).resolve().parent
 
-    # 2. API 요청 URL 구성
-    base_url = 'http://apis.data.go.kr/1360000/SatlitImgInfoService/getInsightSatlit'
+    # 어제 날짜
+    date = ( datetime.today() - timedelta(days=1) ).strftime('%Y%m%d')
 
-    date = "20240616"
-    yesterday = datetime.today() - timedelta(days=1)
-    date = ( yesterday ).strftime('%Y%m%d')
-
-    area = "ko"
-    image_klass = "ir105"
-
-    print( f"date = {date}, area = {area}, image_klass = {image_klass}" )
+    print( f"date = {date}, area = {area}, img_gb = {img_gb}" )
 
     dateYmd = date[0:4] + "-" + date[4:6] + "-" + date[6:]
 
-    dataDir = srcDir.joinpath( "data" )
-    dataDir.mkdir(parents=1, exist_ok=1)
+    if 0 : 
+        dataDir = srcDir.joinpath( "data" )
+        dataDir.mkdir(parents=1, exist_ok=1)
 
-    if not dataDir.exists() or not dataDir.is_dir() :
-        print( f"데이터 폴더가 없습니다." )
+        if not dataDir.exists() or not dataDir.is_dir() :
+            print( f"데이터 폴더가 없습니다." )
 
-        return -1
+            return -1
+        pass
     pass
 
-    imgDir = dataDir.joinpath( dateYmd )
+    imgDir = srcDir.joinpath( f"data/{dateYmd}/{area}/{img_gb}" )
     imgDir.mkdir(parents=1, exist_ok=1)
 
     if not imgDir.exists() or not imgDir.is_dir() :
@@ -128,17 +126,21 @@ def load_satellite_image( api_key ) :
         return -1
     pass
 
+    # 2. API 요청 URL 구성
+    base_url = 'http://apis.data.go.kr/1360000/SatlitImgInfoService/getInsightSatlit'
+
+    # URL 파라미터
     params = {
         'serviceKey': api_key,
         'numOfRows': 10000,  # 한 번에 조회할 데이터 수
         'pageNo': 1,      # 페이지 번호
         'dataType': 'JSON',  # 데이터 타입 (JSON 또는 XML)
         'sat': 'g2',       # 위성 종류 (예: G2 - 천리안2A)
-        # 영상구분 : 적외영상(ir105), 가시영상(vi006), 수증기영상(wv069), 단파적외영상(sw038), 
-        #           RGB컬러(rgbt), RGB주야간합성(rgbdn)
-        'data' : image_klass,
-        'area' : area, # 지역구분 : 전구(fd), 동아시아(ea), 한반도(ko) 
-        'time' : date, # 조회 날짜 (예: 2024년 1월 1일) 
+        'data' : img_gb,  # 영상구분 : 적외영상(ir105), 가시영상(vi006),
+                             #           수증기영상(wv069), 단파적외영상(sw038),
+                             # RGB컬러(rgbt), RGB주야간합성(rgbdn)
+        'area' : area, # 지역구분 : 전구(fd), 동아시아(ea), 한반도(ko)
+        'time' : date, # 조회 날짜 (예: 2024년 1월 1일)
     }
 
     # 3. API 요청 보내기
@@ -155,13 +157,14 @@ def load_satellite_image( api_key ) :
     jsonData = None 
     
     try :
-        jsonData = response.json() 
+        jsonData = response.json()
     except:
         pass
     pass
 
     if jsonData is None :
         print( "Response fail. 잠시후 다시 시도하세요." ) 
+        print( response.text )
 
         return -1
     pass
@@ -201,28 +204,79 @@ def load_satellite_image( api_key ) :
 
     imgUrlsLen = len( imgUrls )
 
+    successCnt = 0 
+
     for i, imgUrl in enumerate( imgUrls ) :
-        try:
-            # HTTP GET 요청을 보내서 이미지 데이터 가져오기
-            response = requests.get( imgUrl )
-            response.raise_for_status()  # HTTP 에러가 발생하면 예외 발생
 
-            imgFileName = imgUrl[ imgUrl.rindex( "/") + 1 : ]
-            imgFile = imgDir.joinpath( imgFileName )
-            # 이미지 데이터를 파일로 저장
-            with open( imgFile, 'wb') as file:
-                file.write(response.content)
+        success = 0
+        imgFileName = imgUrl[ imgUrl.rindex( "/") + 1 : ]
+        imgFile = imgDir.joinpath( imgFileName )
 
-                print( f"[{i +1:4d}][{ (i+1)/imgUrlsLen:6.1%}] 이미지가 성공적으로 저장되었습니다: { imgFileName }" )
+        tryCnt = 0 
+
+        header = f"[{i +1:4d}][{ (i+1)/imgUrlsLen:6.1%}][{tryCnt}]"
+
+        while tryCnt < 10 and ( not success ) :
+
+            tryCnt += 1
+            header = f"[{i +1:4d}][{ (i+1)/imgUrlsLen:6.1%}][{tryCnt}]"
+
+            if tryCnt > 2 :
+                print( f"{header} 영상 파일 다운로드 재시도 중입니다." )
+                print()
             pass
-            
-        except Exception as e:
-            print( f"[{i:04d}] 이미지 다운로드 중 오류가 발생했습니다: {e}" )
+        
+            try:
+                print( f"{header} 이미지를 다운로드 중입니다. { imgFileName }" )
+
+                # HTTP GET 요청을 보내서 이미지 데이터 가져오기
+                response = requests.get( imgUrl )
+                response.raise_for_status()  # HTTP 에러가 발생하면 예외 발생
+
+                content_type = response.headers.get('Content-Type')
+                status_code = response.status_code
+
+                if debug :
+                    print( f"response: ok = {response.ok}, status_code = {status_code}",  )
+                    print( f"response content: type = { content_type } ", f"length = { len( response.content ) } " )
+                pass
+
+                if status_code not in [ 200,  '200' ] :
+                    print( header, f"상태 코드가 적절하지 않습니다. status_code = {status_code}" )
+                elif "image" not in content_type : 
+                    print( header, f"이미지 타입이 아닙니다.", content_type )
+                elif "image" in content_type : 
+                    # 이미지 데이터를 파일로 저장
+                    with open( imgFile, 'wb') as file:
+                        file.write( response.content )
+
+                        print( f"{header} 이미지가 성공적으로 저장되었습니다: { imgFileName }" )
+                    pass
+
+                    success = 1
+                    successCnt += 1
+                pass
+                
+            except Exception as e:
+                success = 0 
+                print( f"{header} 이미지 다운로드 중 오류가 발생했습니다: {e}" )
+
+                sleep( 0.5 ) 
+            pass
         pass
+
+        if not success :
+            print( f"{header} 이미지를 다운로드 실패 : { imgFileName }" )
+        pass
+
     pass
 
-    completeTxt.touch( exist_ok=True )
-    print(f"완료 파일이 생성되었습니다. { completeTxt.name }")
+    if successCnt != len( imgUrls ) : 
+        print( f"완료 파일이 생성되지 않았습니다. {len(imgUrls) - successCnt}개의 영상 파일 다운로드 실패." )
+    elif successCnt == len( imgUrls ) : 
+        completeTxt.touch( exist_ok=True )
+        print( f"완료 파일이 생성되었습니다. { completeTxt.name }" )
+    pass
 
 pass
 
@@ -230,11 +284,17 @@ if __name__ == "__main__" :
 
     # 1. 기상청 API 키 설정
     # 기상청에서 발급받은 API 키를 입력하세요.
-    API_KEY = "YOUR_API_KEY" 
-    API_KEY = "VYvCIN07GWWXrxMyV7Gyyqs%2Bp7acaRleqrBZntsNR5%2F5Bwpy5H4uwE%2B7Rz2tiQWjSKttTX1QgapIc8hJQ1szRw%3D%3D"
+    #API_KEY = "YOUR_API_KEY" 
+    API_KEY = "VYvCIN07GWWXrxMyV7Gyyqs+p7acaRleqrBZntsNR5/5Bwpy5H4uwE+7Rz2tiQWjSKttTX1QgapIc8hJQ1szRw=="
 
     if 1 : 
-        load_satellite_image( API_KEY )
+        # 지역구분 'area' : 전구(fd), 동아시아(ea), 한반도(ko) 
+        for area in [ "ko", "ea", "fd" ] :        
+            # 영상구분 'data' : 적외영상(ir105), 가시영상(vi006),  수증기영상(wv069), 단파적외영상(sw038) RGB컬러(rgbt), RGB주야간합성(rgbdn)
+            for img_gb in [ "ir105", "vi006", "wv069", "sw038", "rgbt", "rgbdn" ] :
+                load_satellite_image( API_KEY, area=area, img_gb=img_gb )
+            pass
+        pass
     pass
 pass
 
