d700e6fa9f459a3a3946b15c03c335e188d9925d
sunabove <sunabove@nate.com>
2024-06-23 10:47:32 +0900

    Create moisture_test.py

---
+++ html/rsc/src/100_Raspberry_Pi/220_DhtSensor/moisture_test.py
@@ -0,0 +1,24 @@
+# moisture_test.py
+from gpiozero import MotionSensor
+
+motionSensor = MotionSensor(14)
+
+count = 0
+def water_detected( ):
+    global count
+    count += 1
+    
+    print( f"[{count:2d}] Water Detected!")  # 물이 감지되었음을 출력
+pass
+
+def water_not_detected( ):
+    global count
+    count += 1
+
+    print( f"[{count:2d}] No Water Detected!")  # 물이 감지되지 않음을 출력
+pass
+
+motionSensor.when_motion = water_detected
+motionSensor.when_no_motion = water_not_detected
+
+input( "Press Enter to quit...\n" )
