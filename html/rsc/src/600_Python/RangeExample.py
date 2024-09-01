c1c8288948725feb39f325a82f92b9c68beea5a1
sunabove <sunabove@nate.com>
2024-09-02 00:06:16 +0900

    Create RangeExample.py

---
+++ html/rsc/src/600_Python/RangeExample.py
@@ -0,0 +1,19 @@
+# 0부터 4까지 출력
+for i in range(5):
+    print(i)
+pass
+
+# 2부터 5까지 출력
+for i in range(2, 6):
+    print(i)
+pass
+
+# 0부터 9까지 2씩 증가하며 출력
+for i in range(0, 10, 2):
+    print(i)
+pass
+
+# 10부터 1까지 2씩 감소하며 출력
+for i in range(10, 1, -2):
+    print(i)
+pass
