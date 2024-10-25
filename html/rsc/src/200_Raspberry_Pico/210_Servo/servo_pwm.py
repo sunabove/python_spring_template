7a8c072977f7160013722dc9376b3bc0d1c5188d
sunabove <sunabove@nate.com>
2024-10-25 17:59:25 +0900

    .

--- html/rsc/src/200_Raspberry_Pico/210_Servo/servo_pwm.py
+++ html/rsc/src/200_Raspberry_Pico/210_Servo/servo_pwm.py
@@ -5,16 +5,13 @@ from time import sleep
 servo = PWM(Pin(1))
 servo.freq(50)  # 서보 모터의 PWM 주파수 설정 (50Hz)
 
-# 최소 및 최대 듀티 사이클 설정 (서보 모터의 최소 및 최대 각도)
-MIN_DUTY = 1638   # 0도 (약 2.5% 듀티 사이클)
-MAX_DUTY = 8192   # 180도 (약 12.5% 듀티 사이클)
-
-duration = 0.1
+duration = 0.5
 count = 0 
-for duty in range( MIN_DUTY, MAX_DUTY + 116, 116 ) :
-    count += 1    
-    print( f"[{count:2d}] duty_u16 = {duty:4d}, duty_cycle ={duty/65535*100:6.2f}%" ) 
-    servo.duty_u16( duty )
+for percent in range( 2, 14 ) :
+    count += 1
+    duty_u16 = 65535*percent//100
+    print( f"[{count:2d}] duty_u16 = {duty_u16:4d}, duty_cycle ={percent:3d}%" ) 
+    servo.duty_u16( duty_u16 )
     sleep( duration )  # 대기 
 pass
 
