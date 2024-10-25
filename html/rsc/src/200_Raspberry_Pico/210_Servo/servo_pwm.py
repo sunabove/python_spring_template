c85a3c20612a0decf8f556a3dbaa5138bb049d85
sunabove <sunabove@nate.com>
2024-10-26 08:30:34 +0900

    Update servo_pwm.py

--- html/rsc/src/200_Raspberry_Pico/210_Servo/servo_pwm.py
+++ html/rsc/src/200_Raspberry_Pico/210_Servo/servo_pwm.py
@@ -5,25 +5,26 @@ from time import sleep  # 대기 기능을 위한 sleep 함수 임포트
 print("Hello ...\n")
 
 # 서보 모터 설정 (핀 1번에 연결)
-servo = PWM(Pin(1))  # 핀 1번에 PWM 신호 설정
-servo.freq(50)  # 서보 모터의 PWM 주파수를 50Hz로 설정 (서보 모터 기본 주파수)
+pwm = PWM(Pin(1))  # 핀 1번에 PWM 신호 설정
+pwm.freq(50)  # 서보 모터의 PWM 주파수를 50Hz로 설정 (서보 모터 기본 주파수)
 
 # 각 듀티 사이클 변경 시 대기 시간
-duration = 0.5  # 0.5초 대기
+duration = 0.25  # 0.5초 대기
 count = 0  # 실행 카운트 초기화
 
-# 듀티 사이클을 2%에서 13%까지 증가시키며 서보 제어
-for percent in range(2, 14):  # 2%부터 13%까지 각도를 증가
+# 듀티 사이클을 2%에서 12.5%까지 증가시키며 서보 제어
+values = list( range(20, 125 + 5, 5) ) # 2%부터 12.5%까지 각도를 증가
+for value in ( values + values[-2:1:-1] ) * 4 :  
     count += 1  # 카운트 증가
-    duty_u16 = 65535 * percent // 100  # 듀티 사이클을 16비트 정수로 변환
-    print(f"[{count:2d}] duty_u16 = {duty_u16:4d}, duty_cycle = {percent:3d}%")  # 현재 듀티 사이클 출력
-    servo.duty_u16(duty_u16)  # 듀티 사이클을 서보에 적용
+    percent = value/1000
+    duty_u16 = int( 65535 * percent ) # 듀티 사이클을 16비트 정수로 변환
+    print(f"[{count:2d}] duty_u16 = {duty_u16:4d}, duty_cycle = {percent*100:4.1f}%")  # 현재 듀티 사이클 출력
+    pwm.duty_u16(duty_u16)  # 듀티 사이클을 서보에 적용
     sleep(duration)  # 설정된 대기 시간 동안 대기
 pass
 
 # 서보 모터 종료
-servo.off()  # 서보 모터의 PWM 신호를 종료
-servo.close()  # PWM을 해제하여 핀 초기화
+pwm.deinit()  # PWM을 해제하여 핀 초기화
 
 # 프로그램 종료 메시지
 print("\nGood bye!")
